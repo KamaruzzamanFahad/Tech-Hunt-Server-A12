e120d479ebbfc27d8fbd73e0e742a2634a4867e6
KamaruzzamanFahad <mihacker41@gmail.com>
2024-06-02 20:48:24 +0600

    verify jwt admin verify token done

--- index.js
+++ index.js
@@ -221,7 +221,6 @@ async function run() {
     //     revinew,
     //   });
     // });
-// gefe
     // app.get('/orderstats',verigytoken,verifyAdmin, async (req, res) => {
     //   const result = await paymentcollection
     //     .aggregate([
